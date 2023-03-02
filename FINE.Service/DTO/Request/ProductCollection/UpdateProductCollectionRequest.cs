176d00a4db106a73a7d941b2bd30b5a77c5e0a8c
Kien Nguyen <78245295+chuthuong13492@users.noreply.github.com>
2023-03-02 14:23:32 +0700

    feat: create product collection service

---
+++ FINE.Service/DTO/Request/ProductCollection/UpdateProductCollectionRequest.cs
@@ -0,0 +1,22 @@
+﻿using FINE.Service.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FINE.Service.DTO.Request.ProductCollection
+{
+    public class UpdateProductCollectionRequest
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? BannerUrl { get; set; }
+        public int StoreId { get; set; }
+        public int Type { get; set; }
+        public int? Position { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
---
+++ FINE.Service/DTO/Response/ProductCollectionResponse.cs
@@ -0,0 +1,25 @@
+﻿using FINE.Service.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FINE.Service.DTO.Response
+{
+    public class ProductCollectionResponse
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? BannerUrl { get; set; }
+        public int StoreId { get; set; }
+        public int Type { get; set; }
+        public int? Position { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime? UpdateAt { get; set; } 
+        public virtual ICollection<ProductResponse> products { get; } = new List<ProductResponse>();
+    }
+}
