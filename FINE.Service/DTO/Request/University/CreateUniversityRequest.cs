5ff6c0ce184fb59e9f320646098caa1addd5291e
Kien Nguyen <78245295+chuthuong13492@users.noreply.github.com>
2023-03-02 14:29:32 +0700

    feat: create university service

---
+++ FINE.Service/DTO/Request/University/CreateUniversityRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINE.Service.DTO.Request.University
+{
+    public class CreateUniversityRequest
+    {
+        public string UniName { get; set; } = null!;
+        public string UniCode { get; set; } = null!;
+        public string? ContactName { get; set; }
+        public string? ContactEmail { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ FINE_API/Controllers/UniversityController.cs
@@ -0,0 +1,68 @@
+﻿using FINE.Service.DTO.Request;
+using FINE.Service.DTO.Request.University;
+using FINE.Service.DTO.Response;
+using FINE.Service.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FINE.API.Controllers
+{
+    [Route(Helpers.SettingVersionApi.ApiVersion)]
+    [ApiController]
+    public class UniversityController : ControllerBase
+    {
+        private readonly IUniversityService _universityService;
+
+        public UniversityController(IUniversityService universityService)
+        {
+            _universityService = universityService;
+        }
+
+        /// <summary>
+        /// Get List Universities
+        /// </summary>
+        ///
+
+        [HttpGet]
+        public async Task<ActionResult<BaseResponsePagingViewModel<UniversityResponse>>> GetUniversities
+            ([FromQuery] UniversityResponse filter, [FromQuery] PagingRequest request)
+        {
+            return await _universityService.GetUniversities(filter, request);
+        }
+
+        /// <summary>
+        /// Get University By Id
+        /// </summary>
+        ///
+
+        [HttpGet("{universityId}")]
+        public async Task<ActionResult<BaseResponseViewModel<UniversityResponse>>> GetUniversityById
+            ([FromRoute] int universityId)
+        {
+            return await _universityService.GetUniversityById(universityId);
+        }
+
+        /// <summary>
+        /// Create University
+        /// </summary>
+        ///
+        [HttpPost]
+        public async Task<ActionResult<BaseResponseViewModel<UniversityResponse>>> CreateUniversity
+            ([FromBody] CreateUniversityRequest request)
+        {
+            return await _universityService.CreateUniversity(request);
+        }
+
+        /// <summary>
+        /// Update University 
+        /// </summary>
+        ///
+
+        [HttpPut("{universityId}")]
+        public async Task<ActionResult<BaseResponseViewModel<UniversityResponse>>> UpdateUniversity
+            ([FromRoute] int universityId, [FromBody] UpdateUniversityRequest request)
+        {
+            return await _universityService.UpdateUnivesity(universityId, request);
+        }
+    }
+}
