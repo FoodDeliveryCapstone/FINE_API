036287eeb8ec68c9feb8aa1ef76dc43642408687
MonMSR <78245295+monsherimsr@users.noreply.github.com>
2023-06-08 11:12:58 +0700

    feat: payment service

---
+++ FINE.Service/Service/PaymentService.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using FINE.Data.UnitOfWork;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINE.Service.Service
+{
+    public interface IPaymentService
+    {
+        void SetPayment();
+    }
+    public class PaymentService : IPaymentService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public PaymentService(IUnitOfWork unitOfWork, IMapper mapper, IConfiguration configuration)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+            _configuration = configuration;
+        }
+
+        public void SetPayment()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex) 
+            {
+
+            }
+        }
+        public void CallMomoService()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(_configuration["Momo:Url"]);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    //try
+                    //{
+
+                    //}
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
