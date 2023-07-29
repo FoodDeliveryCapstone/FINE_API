e36c2cbfe3d8721c6bee00b4b357aceca998c942
MonMSR <78245295+monsherimsr@users.noreply.github.com>
2023-07-29 15:52:35 +0700

    hotfix

--- FINE.Service/Service/OrderService.cs
+++ FINE.Service/Service/OrderService.cs
@@ -118,7 +118,7 @@ namespace FINE.Service.Service
                     var productInMenu = _unitOfWork.Repository<ProductInMenu>().GetAll()
                         .Include(x => x.Menu)
                         .Include(x => x.Product)
-                        .Where(x => x.ProductId == orderDetail.ProductInMenuId)
+                        .Where(x => x.ProductId == orderDetail.ProductId)
                         .FirstOrDefault();
 
                     if(productInMenu == null)
