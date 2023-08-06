cf3398b6e9d194688bdfb28acd50126737aa32a2
MonMSR <78245295+monsherimsr@users.noreply.github.com>
2023-08-06 20:13:16 +0700

    nothing

--- FINE_API/Controllers/OrderController.cs
+++ FINE_API/Controllers/OrderController.cs
@@ -99,14 +99,14 @@ namespace FINE.API.Controllers
         {
             try
             {
-                //var accessToken = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
-                //var customerId = FireBaseService.GetUserIdFromHeaderToken(accessToken);
+                var accessToken = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+                var customerId = FireBaseService.GetUserIdFromHeaderToken(accessToken);
 
-                //if (customerId == null)
-                //{
-                //    return Unauthorized();
-                //}
-                var customerId = "3D596DBF-E43E-45E6-85DD-50CD1095E362";
+                if (customerId == null)
+                {
+                    return Unauthorized();
+                }
+                //var customerId = "3D596DBF-E43E-45E6-85DD-50CD1095E362";
                 return await _orderService.CreateCoOrder(customerId, request);
             }
             catch (ErrorResponse ex)
@@ -137,21 +137,5 @@ namespace FINE.API.Controllers
                 return BadRequest(ex.Error);
             }
         }
-
-        ///// <summary>
-        ///// Update Order Status
-        ///// </summary>
-        //[HttpPut("orderStatus")]
-        //public async Task<ActionResult<BaseResponseViewModel<dynamic>>> UpdateOrderStatus(int orderId, UpdateOrderTypeEnum orderStatus, UpdateOrderRequest request)
-        //{
-        //    try
-        //    {
-        //        return await _orderService.UpdateOrder(orderId, orderStatus, request);
-        //    }
-        //    catch (ErrorResponse ex)
-        //    {
-        //        return BadRequest(ex.Error);
-        //    }
-        //}
     }
 }
