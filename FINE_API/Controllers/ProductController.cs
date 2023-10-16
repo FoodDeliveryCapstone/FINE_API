55a1ad61b4e6988ce1ba373fa76b8b6eed84da85
MonMSR <78245295+monsherimsr@users.noreply.github.com>
2023-10-16 15:49:41 +0700

    fix: dọn code

--- FINE_API/Controllers/ProductController.cs
+++ FINE_API/Controllers/ProductController.cs
@@ -18,20 +18,9 @@ public class ProductController : Controller
         _productService = productService;
     }
 
-    ///// <summary>
-    ///// Get List Product
-    ///// </summary>
-    //[HttpGet]
-    //public async Task<ActionResult<BaseResponsePagingViewModel<ProductResponse>>> GetProducts([FromQuery] ProductResponse request, [FromQuery] PagingRequest paging)
-    //{
-    //    var rs = await _productService.GetProducts(request, paging);
-    //    return Ok(rs);
-    //}
-
     /// <summary>
     /// Get Product By Id
     /// </summary>
-
     [HttpGet("{id}")]
     public async Task<ActionResult<BaseResponseViewModel<ProductResponse>>> GetProductById([FromRoute] string id)
     {
@@ -44,36 +33,4 @@ public class ProductController : Controller
             throw ex;
         }
     }
-
-    ///// <summary>
-    ///// Get List Product By StoreId
-    ///// </summary>
-
-    //[HttpGet("store/{storeId}")]
-    //public async Task<ActionResult<BaseResponsePagingViewModel<ProductResponse>>> GetProductsByStoreId([FromRoute] int storeId, [FromQuery] PagingRequest paging)
-    //{
-    //    return await _productService.GetProductByStore(storeId, paging);
-    //}
-
-    /////<summary>
-    /////Get product By CategoryId
-    ///// </summary>
-    //[HttpGet("category/{categoryId}")]
-    //public async Task<ActionResult<BaseResponsePagingViewModel<ProductResponse>>> GetProductByCategory(
-    //    [FromRoute] int categoryId, [FromQuery] PagingRequest paging)
-    //{
-    //    return await _productService.GetProductByCategory(categoryId, paging);
-    //}
-
-    ///// <summary>
-    ///// Get List Product By MenuId
-    ///// </summary>
-
-    //[HttpGet("menu/{menuId}")]
-    //public async Task<ActionResult<BaseResponsePagingViewModel<ProductInMenuResponse>>> GetProductsByMenuId([FromRoute] int menuId, [FromQuery] PagingRequest paging)
-    //{
-    //    return await _productService.GetProductByMenu(menuId, paging);
-    //}
-
-
 }
