4da857386bae9fc7a71880f2ff5f75eef743d5e2
Hoang Minh <97202270+Cyclops125@users.noreply.github.com>
2023-11-22 13:34:04 +0700

    fix: import product

---
+++ FINE.Service/DTO/Response/ImportResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINE.Service.DTO.Response
+{
+    public class ImportResponse
+    {
+        public List<int>? ErrorLine { get; set; }
+    }
+}
